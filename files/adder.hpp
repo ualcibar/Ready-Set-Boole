0bbe7850836e1c5fd08aad4b52956847406e1096
Unai Alcibar <ualcibar11@gmail.com>
2026-09-23 10:52:16 +0200

    Progress

---
+++ files/adder.hpp
@@ -0,0 +1,12 @@
+#ifndef ADDER_HPP
+#define ADDER_HPP
+
+#include <iostream>
+#include <cstdint>
+
+// Suma a + b usando SOLO: & | ^ << >> = == != < > <= >=
+// (++ o += 1 solo permitido para el índice de un bucle, no para el resultado)
+uint32_t adder(uint32_t a, uint32_t b);
+uint32_t adder2bit(uint32_t a, uint32_t b);
+
+#endif
---
+++ files/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "adder.hpp"
+
+int main()
+{
+    struct TestCase { uint32_t a; uint32_t b; uint32_t expected; };
+    /*
+    TestCase tests[] = {
+        {5, 2, 7},
+        {7, 1, 8},
+        {10, 6, 16},
+        {13, 11, 24},
+        {31, 1, 32},
+        {11, 11, 22},
+        {0, 0, 0},
+        {0, 5, 5},
+    };
+    int failed = 0;
+    int cont = 0;
+    */
+
+    std::cout << "Result of test \n" << adder(6, 1) << " <-Final Result\n";
+    /*
+    for (const auto& t : tests)
+    {
+        cont++;
+        std::cout << "Estamos en el " << cont << " test!\n";
+        uint32_t result = adder(t.a, t.b);
+        bool ok = (result == t.expected);
+        std::cout << t.a << " + " << t.b << " = " << result
+        << " (esperado " << t.expected << ") "
+        << (ok ? "OK" : "FALLO") << '\n';
+        if (!ok) failed++;
+    }
+    
+    std::cout << '\n' << (failed == 0 ? "Todos los tests pasan." :
+    std::to_string(failed) + " test(s) fallaron.") << '\n';
+    return failed == 0 ? 0 : 1;
+    */
+    return 1;
+}
