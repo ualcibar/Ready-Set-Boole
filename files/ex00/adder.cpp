4bb6fc197fa3e82429742444ca2018da4896f0c2
Unai Alcibar <ualcibar11@gmail.com>
2026-09-23 16:53:22 +0200

    clean ex00

--- files/ex00/adder.cpp
+++ files/ex00/adder.cpp
@@ -14,11 +14,8 @@ uint32_t adder(uint32_t a, uint32_t b) {
     uint32_t result_bit;
     uint32_t result = 0;
     uint32_t c_in = 0;
-    int i = 1;
+    int i = 0;
 
-    addOperation(extract_first_bit(a), extract_first_bit(b), &result, &c_in);
-    a = a >> 1;
-    b = b >> 1;
     while (a != 0 || b != 0 || c_in != 0) {
         addOperation(extract_first_bit(a), extract_first_bit(b), &result_bit, &c_in);
         result = result | result_bit << i;
@@ -27,21 +24,4 @@ uint32_t adder(uint32_t a, uint32_t b) {
         i++;
     }
     return result;
-}
-
-
-uint32_t adder2bit(uint32_t a, uint32_t b) {
-    uint32_t a_bit;
-    uint32_t b_bit;
-    uint32_t s_bit;
-    uint32_t s = 0;
-
-    a_bit = extract_first_bit(a);
-    b_bit = extract_first_bit(b);
-    s_bit = a_bit ^ b_bit;
-    s = s | s_bit;
-
-    a = a >> 1;
-    b = b >> 1;
-    return s;
 }
--- files/ex00/adder.hpp
+++ files/ex00/adder.hpp
@@ -4,9 +4,9 @@
 #include <iostream>
 #include <cstdint>
 
-// Suma a + b usando SOLO: & | ^ << >> = == != < > <= >=
-// (++ o += 1 solo permitido para el índice de un bucle, no para el resultado)
 uint32_t adder(uint32_t a, uint32_t b);
-uint32_t adder2bit(uint32_t a, uint32_t b);
+void addOperation(uint32_t a, uint32_t b, uint32_t *result, uint32_t *c_in);
+uint32_t extract_first_bit(uint32_t num);
+
 
 #endif
